b19ca6e712c62cf3a5e30e0c30a1d5733ca8fd93
Sungho Park <siminsungho@gmail.com>
2019-11-04 03:51:38 +0900

    Add default files

---
+++ app/ex03stm32f2_nucleof207zg.cmake
@@ -0,0 +1,5 @@
+include(${PROJECT_UBINOS_DIR}/config/ubinos_nucleof207zg.cmake)
+
+include(${CMAKE_CURRENT_LIST_DIR}/ex03stm32f2.cmake)
+
+
---
+++ app/ex03stm32f2_stm3221geval.cmake
@@ -0,0 +1,5 @@
+include(${PROJECT_UBINOS_DIR}/config/ubinos_stm3221geval.cmake)
+
+include(${CMAKE_CURRENT_LIST_DIR}/ex03stm32f2.cmake)
+
+
