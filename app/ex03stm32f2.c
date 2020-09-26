f53ec5453e7033bf7937a21e87e4194cbb6b374c
Sungho Park <siminsungho@gmail.com>
2020-09-27 00:15:14 +0900

    refactoring for auto carriage return feature

--- app/ex03stm32f2.c
+++ app/ex03stm32f2.c
@@ -14,30 +14,22 @@ static void helloworld_mt_task2func(void * arg);
 int appmain(int argc, char * argv[]) {
 	int r;
 
-	printf("\n\n\n\r");
-	printf("================================================================================\n\r");
-	printf("helloworld_mt (multi-tasking) (build time: %s %s)\n\r", __TIME__, __DATE__);
-	printf("================================================================================\n\r");
-    printf("\n\r");
-#if (UBINOS__UBICLIB__USE_MALLOC_RETARGETING == 1)
-    r = heap_printheapinfo(NULL);
-    if (0 == r) {
-        printf("\n\r");
-        printf("================================================================================\n\r");
-        printf("\n\r");
-    }
-#endif /* (UBINOS__UBICLIB__USE_MALLOC_RETARGETING == 1) */
+	printf("\n\n\n");
+	printf("================================================================================\n");
+	printf("helloworld_mt (multi-tasking) (build time: %s %s)\n", __TIME__, __DATE__);
+	printf("================================================================================\n");
+    printf("\n");
 
     srand(time(NULL));
 
 	r = task_create(NULL, helloworld_mt_task1func, NULL, task_getmiddlepriority(), 0, "task1");
 	if (0 != r) {
-		logme("fail at task_create\r\n");
+		logme("fail at task_create\n");
 	}
 
 	r = task_create(NULL, helloworld_mt_task2func, NULL, task_getmiddlepriority(), 0, "task2");
     if (0 != r) {
-        logme("fail at task_create\r\n");
+        logme("fail at task_create\n");
     }
 
 	ubik_comp_start();
@@ -51,11 +43,11 @@ static void helloworld_mt_task1func(void * arg) {
 
 	(void) r;
 
-    printf("\n\r");
+    printf("\n");
 
 	for (unsigned int i = 0; ; i++) {
 	    delayms = (rand() % 10 + 1) * 200;
-		printf("1: hello world ! (%u) (delay = %4d ms)\n\r", i, delayms);
+		printf("1: hello world ! (%u) (delay = %4d ms)\n", i, delayms);
 		task_sleep(delayms);
 	}
 }
@@ -66,11 +58,11 @@ static void helloworld_mt_task2func(void * arg) {
 
     (void) r;
 
-    printf("\n\r");
+    printf("\n");
 
     for (unsigned int i = 0; ; i++) {
         delayms = (rand() % 10 + 1) * 200;
-        printf("2: hello world ! (%u) (delay = %4d ms)\n\r", i, delayms);
+        printf("2: hello world ! (%u) (delay = %4d ms)\n", i, delayms);
         task_sleep(delayms);
     }
 }
